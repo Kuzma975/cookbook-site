9090e6c52141847a0e87bd107475d3fecd312ea4
Kuzma Rezchenko <kuzma_rezchenko@epam.com>
2017-01-19 15:54:29 +0000

    add links to metadata.rb in server cookbook

--- cookbooks/server/metadata.rb
+++ cookbooks/server/metadata.rb
@@ -5,5 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.1'
+issues_url       'https://github.com/Kuzma975/cookbook-site/issues'
+source_url       'https://github.com/Kuzma975/cookbook-site'
 
 depends          'nginx', '~> 2.7.6'
