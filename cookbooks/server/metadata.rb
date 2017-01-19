0993829309763d690f66ac09e1dd24b496da97df
Kuzma Rezchenko <kuzma_rezchenko@epam.com>
2017-01-19 16:15:13 +0000

    increment version server cookbook

--- cookbooks/server/metadata.rb
+++ cookbooks/server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kuzma_rezchenko@epam.com'
 license          'All rights reserved'
 description      'Installs/Configures server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 issues_url       'https://github.com/Kuzma975/cookbook-site/issues'
 source_url       'https://github.com/Kuzma975/cookbook-site'
 
